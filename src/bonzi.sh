ba4d0419fe9b19bad81ddee4205da1335dba7033
Liudvikas Taluntis <liudvikast@gmail.com>
2020-09-11 14:49:05 +0100

    Made bonzi get printed in a loop.
    Counter based loop for now - easier to work with during dev.
    Extracted bonzi printing into a 'function' for better readability.

--- src/bonzi.sh
+++ src/bonzi.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+count=20
 BASEDIR=$(dirname "$0")
 
-cat $BASEDIR/../data/images/hands-open.txt
+function PrintBonzi(){
+    cat $BASEDIR/../data/images/hands-open.txt
+}
+
+while [ $count -gt 0 ]
+do 
+    sleep 5
+    PrintBonzi
+    echo "Hello, my name is Bonzi!"
+    count=`expr $count - 1`
+done
+
